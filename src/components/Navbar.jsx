67b648cfdf01c4df7fcc13aafd0e18824bc92858
E-code804 <erikpfeffer5@gmail.com>
2023-07-20 21:58:09 -0400

    Initial push + navbar creation.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "../assets/menu.png"
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <div className="flex justify-center items-center cursor-pointer">
+          <img width="45" height="45" src={logo} alt="logo" />
+          <p className="nav-logo-text">Menu Master</p>
+        </div>
+
+        <button type="button" className="custom-btn sign-in">Sign In</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
